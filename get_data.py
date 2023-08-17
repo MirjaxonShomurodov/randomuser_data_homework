1263b17d29ffa0a2e5ebb84d6002d94b0f915d08
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-08-17 12:32:47 +0500

    get_arr.append(data[messages][i][from_id]) ~data

--- get_data.py
+++ get_data.py
@@ -1,5 +1,5 @@
 import json
-
+from pprint import pprint
 def get_data(filename:str) -> dict:
     """
     You are given a filename. Read the JSON data from the file and return the dictionary.
@@ -9,3 +9,9 @@ def get_data(filename:str) -> dict:
     Returns:
         dict: JSON data
     """
+   
+    filename=json.dumps(randomuser_data)
+    return filename
+f=open('randomuser_data.json','r')
+randomuser_data=f.read()
+pprint(get_data(f))
