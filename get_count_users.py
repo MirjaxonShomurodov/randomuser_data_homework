9cf88e87327777b7c7bab5feefd9eedb0aeb6131
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-08-17 12:31:50 +0500

    users

--- get_count_users.py
+++ get_count_users.py
@@ -9,4 +9,13 @@ def get_count_users(data:dict) -> int:
     Returns:
         int: number of users
     """
+data= {
+        1:"1_krs",
+        2:"2_krs",
+        3:"3_krs",
+        4:"4_krs"
+    }
+for i in data:
+        print(i+1)
+print(get_count_users(data))
     
